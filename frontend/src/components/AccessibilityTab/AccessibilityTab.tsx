3a4c53e3c39efc37aec237672e352431abf6d183
MateusLeffeck <mateusleffeck@gmail.com>
2024-05-12 23:44:39 -0300

    Versao VLibras feio

--- frontend/src/components/AccessibilityTab/AccessibilityTab.tsx
+++ frontend/src/components/AccessibilityTab/AccessibilityTab.tsx
@@ -1,43 +1,36 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 import './accessibilitytab.css';
+
+import VLibras from '@djpfs/react-vlibras';
 
 import { lensDown, lensUp, darkThemeIcon, signLanguageIcon, accessIcon } from '../../assets';
 
-import {  fontSizes } from '../../types/interfacePageProps'
-
-
+import { fontSizes } from '../../types/interfacePageProps';
 
 interface AccessibilityProps {
-  currentTheme?: string,
-  setCurrentTheme : (args0: string) => void,
-  fontSizes ?: fontSizes,
-  setFontSizes : (args0 : number)=> void
-  
+  currentTheme?: string;
+  setCurrentTheme: (args0: string) => void;
+  fontSizes?: fontSizes;
+  setFontSizes: (args0: number) => void;
 }
 
-function AccessibilityTab( { currentTheme, setCurrentTheme, fontSizes, setFontSizes} : AccessibilityProps) {
-  // const [theme, setTheme] = useState('light');
-
-  // event handler do botao do modo noturno
+function AccessibilityTab({ currentTheme, setCurrentTheme, fontSizes, setFontSizes }: AccessibilityProps) {
   const changeThemeCallback = () => {
-    if (currentTheme==='light') {
+    if (currentTheme === 'light') {
       setCurrentTheme('dark');
     } else {
-      setCurrentTheme('light')
+      setCurrentTheme('light');
     }
-  }
+  };
 
   const increaseFontCallback = () => {
-    setFontSizes(1)    ;
-
-  }
+    setFontSizes(1);
+  };
 
   const decreaseFontCallback = () => {
     setFontSizes(0);
-  }
+  };
 
-  // troca de temas dos componentes genericos (header, background)
-  // deve ser feito nesta ordem, pois useEffect sempre executa uma vez no carregamento da pagina
   useEffect(()=> {
     if (currentTheme === 'dark') {
       document.documentElement.style.setProperty('--main-color', 'var(--header-dark)');
@@ -56,26 +49,33 @@ function AccessibilityTab( { currentTheme, setCurrentTheme, fontSizes, setFontSi
     }
   }, [currentTheme]);
   
+
   return (
     <>
-        <div className='accessContainer'>
-            <div className='accessContainer--buttons'>
-                <img className='signlLanguageIcon' src={signLanguageIcon} alt="Mãos indicando libras" />
+      <div className='accessContainer'>
+        <div className='accessContainer--buttons'>
+          <VLibras/>
+          <button className='nightModeButton' onClick={changeThemeCallback}>
+            <img className='nightModeIcon' src={darkThemeIcon.toString()} alt='Simbolo modo noturno' />
+            Modo noturno
+          </button>
 
-                <button className='nightModeButton' onClick={ changeThemeCallback}> <img className='nightModeIcon' src={darkThemeIcon.toString()} alt="Simbolo modo noturno" /> Modo noturno</button>
+          <button className='increaseFontButton' onClick={increaseFontCallback}>
+            <img className='increaseFontIcon' src={lensUp.toString()} alt='Lupa com aumento' />
+            Aumentar fonte
+          </button>
 
-                <button className='increaseFontButton' onClick={increaseFontCallback}> <img className='increaseFontIcon' src={lensUp.toString()} alt="Lupa com aumento" />Aumentar fonte</button>
-
-                <button className='decreaseFontButton' onClick={decreaseFontCallback}> <img className='decreaseFontIcon' src={lensDown.toString()} alt="Lupa com diminuição" />Diminuir fonte</button>
-
-            </div>
-            <div className='accessContainer--border'>
-                <img src={accessIcon.toString()} alt="Accessibilidade" />
-            </div>
-
+          <button className='decreaseFontButton' onClick={decreaseFontCallback}>
+            <img className='decreaseFontIcon' src={lensDown.toString()} alt='Lupa com diminuição' />
+            Diminuir fonte
+          </button>
         </div>
+        <div className='accessContainer--border'>
+          <img src={accessIcon.toString()} alt='Accessibilidade' />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default AccessibilityTab;
