4211f4eff093727c479406b4fb624a2937a36723
joseCarlosAndrade <josecarlos.101@usp.br>
2024-05-12 22:02:07 -0300

    fixing conflicts

--- frontend/src/components/sections/SectionSendFileClient/SectionSendFileClient.tsx
+++ frontend/src/components/sections/SectionSendFileClient/SectionSendFileClient.tsx
@@ -19,14 +19,20 @@ function SectionSendFileClient() {
 
             
             <div className='sendFile--productType'>
-                <text className='sendFile--productType-text'></text>
+                {/* <text className='sendFile--productType-text'></text> */}
                 <div className='sendFile--productType-dropbox'>
-                    <text className='sendFile--productType-dropbox-dropbtn'>Selecione o tipo de produto aqui! É tudo muito fácil.</text>
-                    <div className='sendFile--productType-dropbox-contents'>
+                    <label htmlFor='product-type' className='sendFile--productType-dropbox-dropbtn'>Selecione o tipo de produto aqui! É tudo muito fácil.</label>
+                    <select name="product-type" id="product-type">
+                        <option value='opcao1'>Opcao 1</option>
+                        <option value='opcao2'>Opcao 2</option>
+                        <option value='opcao3'>Opcao 3</option>
+                    </select>
+                        
+                        {/* <div className='sendFile--productType-dropbox-contents'>
                         <button>Opcao 1</button>
                         <button>Opcao 2</button>
                         <button>Opcao 3</button>
-                    </div>
+                    </div> */}
                 </div>
             </div>
 
@@ -79,7 +85,7 @@ function SectionSendFileClient() {
 
             <div className='sendFile--send'>
                 <div className='sendFile--send-text'>Agora é só clicar em enviar</div>
-                <button className='sendFile--send-button'>Enviar</button>
+                <button className='sendFile--send-button' onClick={() => {alert("Dados enviados com sucesso!")}}>Enviar</button>
             </div>
         </div>
     </>
