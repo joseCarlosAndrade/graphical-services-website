44188dc73fb7076a4d362eea89d5ff132187d29d
joseCarlosAndrade <josecarlos.101@usp.br>
2024-05-22 18:40:02 -0300

    font limiting factor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -23,8 +23,8 @@ function App() {
 
   const [fontSizesHolder, setFontSizesHolder] = useState<fontSizes>({
     accessButtonFont: 1.2,
-    headerFont: 0.8,
-    homepageFont: 0.9,
+    headerFont: 1,
+    homepageFont: 1,
     footerFont: 1,
     servicePageFont: 1,
     productPageFont: 1,
@@ -32,20 +32,28 @@ function App() {
     sendFilePageFont: 1,
     whoAreWePageFont: 1.3
   });
-
+  const [limitFontSize, setLimitFontSize] = useState(0);
 
   const changeFont = (i: number) => {
     const changeFactor: number = 0.2
+    if (i === 1 ) {
+      if (limitFontSize >= 3) return;
+      setLimitFontSize(prev => prev+1);
+      
+    } else {
+      if (limitFontSize <= -3) return;
+      setLimitFontSize(prev => prev-1);
+    }
     setFontSizesHolder(prevState => ({
-      accessButtonFont: prevState.accessButtonFont + (i === 1 ? changeFactor : -changeFactor),
-      headerFont: prevState.headerFont + (i === 1 ? changeFactor : -changeFactor),
-      homepageFont: prevState.homepageFont + (i === 1 ? changeFactor : -changeFactor),
-      servicePageFont: prevState.accessButtonFont + (i === 1 ? changeFactor : -changeFactor),
-      productPageFont: prevState.accessButtonFont + (i === 1 ? changeFactor : -changeFactor),
-      footerFont: prevState.footerFont + (i === 1 ? changeFactor : -changeFactor),
-      sendFilePageFont: prevState.footerFont + (i === 1 ? changeFactor : -changeFactor),
-      loginFont: prevState.loginFont + (i === 1 ? changeFactor : -changeFactor),
-      whoAreWePageFont: prevState.loginFont + (i === 1 ? changeFactor : -changeFactor)
+      accessButtonFont: prevState.accessButtonFont + (i === 1 ? (changeFactor) : -(changeFactor)),
+      headerFont: prevState.headerFont + (i === 1 ? (changeFactor) : -(changeFactor)),
+      homepageFont: prevState.homepageFont + (i === 1 ? (changeFactor) : -(changeFactor)),
+      servicePageFont: prevState.accessButtonFont + (i === 1 ? (changeFactor) : -(changeFactor)),
+      productPageFont: prevState.accessButtonFont + (i === 1 ? (changeFactor) : -(changeFactor)),
+      footerFont: prevState.footerFont + (i === 1 ? (changeFactor) : -(changeFactor)),
+      sendFilePageFont: prevState.footerFont + (i === 1 ? (changeFactor) : -(changeFactor)),
+      loginFont: prevState.loginFont + (i === 1 ? (changeFactor) : -(changeFactor)),
+      whoAreWePageFont: prevState.loginFont + (i === 1 ? (changeFactor) : -(changeFactor))
     }));
   }
 
