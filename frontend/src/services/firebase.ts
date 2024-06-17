104a03a7f51d0bd852cd3ac1d97f856f8c4a8d39
shogoshima <shogoshima@usp.br>
2024-06-17 13:42:56 -0300

    solved issue of .env in react app

--- frontend/src/services/firebase.ts
+++ frontend/src/services/firebase.ts
@@ -3,20 +3,17 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-// import dotenv from 'dotenv'
-// dotenv.config();
-// console.log("KEY:", process.env.API_KEY)
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyA_CW8unJbJovox1QVIJGQRzne2nQcB82E",
-    authDomain: "graphical-services-test.firebaseapp.com",
-    projectId: "graphical-services-test",
-    storageBucket: "graphical-services-test.appspot.com",
-    messagingSenderId: "337564264216",
-    appId: "1:337564264216:web:4b10f8bd406b1957c3606d",
-    measurementId: "G-M1S6HCMG0P"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASURAMENT_ID
 };
 
 // Initialize Firebase
